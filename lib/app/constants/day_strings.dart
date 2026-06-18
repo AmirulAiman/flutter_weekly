0e9640b5e4ce4e16e4188dca8dcbe3bab5ce7ed2
Amirul Aiman <aiman_amirul@ymail.com>
2026-06-18 14:21:09 +0800

    chore: add days and months constant string

---
+++ lib/app/constants/day_strings.dart
@@ -0,0 +1,13 @@
+class DayStrings {
+  static List<String> get days => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  static List<String> get dayFullNames => [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+}
---
+++ lib/app/constants/month_strings.dart
@@ -0,0 +1,31 @@
+class MonthStrings {
+  static List<String> get months => [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  static List<String> get monthFullNames => [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+}
