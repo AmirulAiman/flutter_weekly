a34f5fb70b2a34d93b9a2cdaa394930d1b652e7c
Amirul Aiman <aiman_amirul@ymail.com>
2026-06-24 17:14:00 +0800

    chore: prep theme file

---
+++ lib/core/themes/app_themes.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppThemes {
+  static final primary = Color(0xffFFFFFF);
+  static final onPrimary = Color(0xffFFFFFF);
+  static final secondary = Color(0xffFFFFFF);
+  static final onSecondary = Color(0xffFFFFFF);
+  static final surface = Color(0xffFFFFFF);
+  static final onSurface = Color(0xffFFFFFF);
+  static final alert = Color(0xffFFFFFF);
+  static final onAlert = Color(0xffFFFFFF);
+  static final info = Color(0xffFFFFFF);
+  static final onInfo = Color(0xffFFFFFF);
+}
